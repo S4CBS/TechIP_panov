cb88e0365b9ba3bd8e9b4afe446bced818c81e4a
S4CBS <107351385+S4CBS@users.noreply.github.com>
2026-09-21 15:51:04 +0300

    chore: prepare practice 01 structure

---
+++ practice-01/js/hello.js
@@ -0,0 +1,12 @@
+// ПР1: первая программа — вывод в консоль
+"use strict";
+
+const studentName = "Alexey Panov";
+const groupName = "EFBO-13-25";
+const practiceNumber = 1;
+
+console.log(`Студент: ${studentName}`);
+console.log(`Группа: ${groupName}`);
+console.log(`Практическая работа: ${practiceNumber}`);
+console.log("JavaScript успешно запущен");
+console.log("Тип document:", typeof document);
